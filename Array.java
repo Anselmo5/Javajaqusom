d5c4a35ba2173f4b91466c332593a191675638a0
ANSELMO HENRIQUE ASSUNÇÃO REIS <anselmo_reis@estudante.sesisenai.org.br>
2023-06-12 09:54:53 -0300

    anselmo

---
+++ Array.java
@@ -0,0 +1,21 @@
+//import java.util.ArrayList;
+
+//public class Array {
+    //Sinta-se array - tipo[] nome = new int quantidade
+
+    //String [] numeros = new int[6]; //sinta-se do array - Uma forma de se fazer
+    //produtos[0] = "maçã";
+    //produtos[1] = "batata";
+    //produtos[2] = "banana";
+
+
+    //String [] produtos = {"maçã", "batata", "banana"}; // Array prenchido - outra forma de fazer
+
+    //ArrayList <String> produtos = new ArrayList<>(); // - Sinta-se do arraylist
+   // produtos.add("maçã"); // adicionando os valores
+   // produtos.add("banana");
+  //  produtos.add("batata");
+
+   // produtos.remove("banana") // quando removemos um produto o produto antesesor ou susesor ira herdar o valor do numero removido.
+    // exemplos: banana e ocupa a posição 1, quando removemos ela a batata vai ocupar o valor 1
+//}
